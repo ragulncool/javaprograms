89d665359ddb2db930bfcf3a14144746eef1ed63
Ragul <raguln.cool@gmail.com>
2019-04-15 23:45:07 +0530

    First

---
+++ Java Programs/src/SerializationDemo.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public class SerializationDemo {
+
+	public static void main(String[] args) throws Exception {
+		Save obj=new Save();
+		obj.i=4;
+		
+		File f=new File("obj.txt");
+		FileOutputStream fos=new FileOutputStream(f);
+		ObjectOutputStream oos=new ObjectOutputStream(fos);
+		oos.writeObject(obj);
+		
+		FileInputStream fis=new FileInputStream(f);
+		ObjectInputStream ois=new ObjectInputStream(fis);
+		Save obj1=(Save)ois.readObject();
+		
+		System.out.println("Obj1 is "+obj1.i);
+
+	}
+
+}
+
+class Save implements Serializable{
+	int i;
+}
